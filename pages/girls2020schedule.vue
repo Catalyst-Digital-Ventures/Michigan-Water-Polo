ed68cf29a08f4ec3e565d93f43741e580d1b544c
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-02-28 13:20:49 -0500

    girls schedule update

--- pages/girls2020schedule.vue
+++ pages/girls2020schedule.vue
@@ -1,49 +1,23 @@
 <template>
   <div>
-    <h1 class="pageTitle">Girls 2020 Schedule</h1>
-
-    <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-      <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
-    </v-tabs>
-    <v-tabs-items v-model="tab">
-      <v-tab-item>
-        <v-row>
-          <v-col v-for="card in eastGames" :key="card.id" cols="12" sm="6" md="4" lg="3">
-            <v-card class="mx-auto" max-width="500" raised>
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <v-list-item-subtitle v-text="card.date"></v-list-item-subtitle>
-                  <v-list-item-title v-text="card.home"></v-list-item-title>
-                  <v-list-item-title v-text="card.away"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-tab-item>
-      <v-tab-item>
-        <v-row>
-          <v-col v-for="card in westGames" :key="card.id" cols="12" sm="6" md="4" lg="3">
-            <v-card class="mx-auto" max-width="500" raised>
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <v-list-item-subtitle v-text="card.date"></v-list-item-subtitle>
-                  <v-list-item-title v-text="card.home"></v-list-item-title>
-                  <v-list-item-title v-text="card.away"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-tab-item>
-    </v-tabs-items>
+    <article class="gsheets-container">
+      <vue-friendly-iframe
+        :src="
+          'https://docs.google.com/spreadsheets/d/e/2PACX-1vR6A8XwIN_Q21i5zSQdP3oIev_-t7edsSWRpzWWqlgQ8SA-f7Ct4EbkSHbvriqve8T5FJRYjBLW_16u/pub?embedded=true'
+        "
+      ></vue-friendly-iframe>
+    </article>
   </div>
 </template>
 
 <script>
 import cards from "../data/2020girlsSchedule.json";
+import VueFriendlyIframe from "vue-friendly-iframe";
 
 export default {
+  components: {
+    VueFriendlyIframe
+  },
   data() {
     return {
       cards,
@@ -65,3 +39,6 @@ export default {
   }
 };
 </script>
+
+
+
