2e491b06cd429d630245850a5b419e61390782fb
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-02-28 13:34:32 -0500

    polices css prop not working

--- pages/policies.vue
+++ pages/policies.vue
@@ -3,7 +3,6 @@
     <h1 class="pageTitle">Polices</h1>
     <v-layout column justify-center align-center>
       <vue-friendly-iframe
-        class="gdocs-container"
         :src="
            'https://docs.google.com/document/d/e/2PACX-1vTbZt6iamk_ov46j9cW2Ls0xcsJVeTvygW3qHnpV0LHoGkhkyTf5W0b_rylKGZI9o95zM1RW-9ZTMrg/pub?embedded=true'
         "
